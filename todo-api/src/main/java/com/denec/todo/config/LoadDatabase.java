4a85cbeff08254192269b5129c60bc622bff0eb8
Denec <dzenevich@mail.ru>
2024-04-07 18:58:27 +0300

    add api consumption

--- todo-api/src/main/java/com/denec/todo/config/LoadDatabase.java
+++ todo-api/src/main/java/com/denec/todo/config/LoadDatabase.java
@@ -1,7 +1,5 @@
 package com.denec.todo.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +9,6 @@ import com.denec.todo.task.TaskRepository;
 
 @Configuration
 public class LoadDatabase {
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-
     @Bean
     CommandLineRunner initDatabase(TaskRepository repository) {
         return args -> {
--- todo-app/src/repositories/taskApi.ts
+++ todo-app/src/repositories/taskApi.ts
@@ -1,23 +1,17 @@
 import type { Task } from '../types/Task';
 
 class TaskApi {
-    tasks: Task[] = [
-        { id: 0, text: 'get up' },
-        { id: 1, text: 'play puter gamez' },
-        { id: 2, text: 'go to sleep' }
-    ];
-
-    constructor() {
-        console.log('add task');
-    }
+    taskEndpointLink: string = 'http://localhost:8080/task';
 
     getTasks(): Promise<Task[]> {
-        return Promise.resolve(this.tasks);
+        return fetch(this.taskEndpointLink)
+        .then(res => res.json())
+        .then(res => {
+          return res as Task[]
+        })
     }
 
     addTask(newTask: Task): Task {
-        newTask.id = this.tasks[this.tasks.length - 1].id + 1;
-        this.tasks.push(newTask);
         return newTask;
     }
 }
