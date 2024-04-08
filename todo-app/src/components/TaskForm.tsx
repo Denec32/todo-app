31a0450c7a85e48fb1f7a70535ffb34fd7593f6e
Denec <dzenevich@mail.ru>
2024-04-08 23:25:40 +0300

    clear input value on submit

--- todo-app/src/components/TaskForm.tsx
+++ todo-app/src/components/TaskForm.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction } from "react";
+import { SetStateAction, useState } from "react";
 import { taskApi } from "../repositories/taskApi";
 import { Task } from "../types/Task";
 
@@ -7,6 +7,13 @@ type TaskFormProps = {
 }
 
 function TaskForm(props: TaskFormProps) {
+    const [inputText, setInputText] = useState<string>("");
+
+    function changeInputText(event: React.ChangeEvent<HTMLInputElement>) {
+        event.preventDefault();
+        setInputText(event.currentTarget.value);
+    }
+
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const target = event.target as typeof event.target & {
@@ -16,11 +23,13 @@ function TaskForm(props: TaskFormProps) {
         const text = target.text.value;
         taskApi.addTask({ id: 0, text: text })
         .then(task => props.setTasks((tasks) => [...tasks, task]));
+
+        setInputText("");
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type='text' name='text'></input>
+            <input onChange={changeInputText} type='text' name='text' value={inputText}></input>
             <button type='submit'>add</button>
         </form>
     )
